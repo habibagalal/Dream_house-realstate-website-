25defa9daa09ac3e4a355a259b8f86e96d82eb93
habibagalal <habibagalal392@gmail.com>
2022-03-27 15:48:33 +0200

    int.

--- test/user/models.py
+++ test/user/models.py
@@ -5,3 +5,5 @@ from django.db import models
 
 class User (models.Model):
     name = models.CharField(max_length=100)
+    password = models.TextField(max_length=100)
+    user = models.IntegerField
